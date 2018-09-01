1f3507b4d13df7f1d9c03231985b1334289ef0c6
juandiegombr <juandiego.mbr@gmail.com>
2018-09-01 23:14:18 +0200

    :sparkles: Init gh-pages

---
+++ precache-manifest.1a509b3046211262c2b4e9254c9dad63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "d73693a436e82bcef063",
+    "url": "/js/chunk-vendors.b0590309.js"
+  },
+  {
+    "revision": "4b1c11c1123212fd6ce4",
+    "url": "/js/app.017988ab.js"
+  },
+  {
+    "revision": "b3cb300ed644d1c259d45165dea05b3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "4b1c11c1123212fd6ce4",
+    "url": "/css/app.d662b95e.css"
+  }
+];
